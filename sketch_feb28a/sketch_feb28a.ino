90c4180fc4920ac30fc18dd4a915db8b5f0e2273
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-02-28 17:25:43 +0700

    ...

--- sketch_feb28a/sketch_feb28a.ino
+++ sketch_feb28a/sketch_feb28a.ino
@@ -1,6 +1,15 @@
+#define INPUT_PIN 89
+#define OUTPUT_PIN 88
+
+
+void IRAM_ATTR Mount(){
+
+}
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(INPUT_PIN, INPUT);
+  pinMode(OUTPUT_PIN, OUTPUT);
 
 }
 
