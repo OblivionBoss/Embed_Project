3bc2d0627c18fec24464307db66a9d44a40d8daf
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-03-02 21:47:42 +0700

    update mqtt

--- sketch_feb28a/sketch_feb28a.ino
+++ sketch_feb28a/sketch_feb28a.ino
@@ -2,59 +2,27 @@
 #define OUTPUT_PIN 88
 
 #include <WiFi.h>
-#include <DHTesp.h>
-#include <MQTT.h>
 #include <ThingsBoard.h>
 
-const char ssid[] = "";
+const char ssid[] = ""; 
 const char password[] = "";
 
 const char mqtt_broker[] = "thingsboard.cloud";
-const char mqtt_Token[] = "";
+const char mqtt_Token[] = "7aKzt3N0249c1ZVtlQfs";
 const char mqtt_topic[] = "v1/devices/me/telemetry";
 // const char mqtt_client_id = "";
 int MQTT_PORT = 1883;
+int status = WL_IDLE_STATUS;
 
 WiFiClient net;
-DHTesp dht;
 // MQTTClient client;
 ThingsBoard tb(net);
 
-RPC_Response processGetVal1(const RPC_Data &data){
-  String respStr="";
-
-  return respStr;
-}
-
-RPC_Response processGetVal2(const RPC_Data &data){
-  String respStr = "";
-
-  return respStr;
-}
-
-RPC_Callback callbacks[]={
-  {"valiable1",},
-  {"valiable2",}
-};
-
 void connect() {
   Serial.print("checking wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  
+  WiFi.begin(ssid,password);
+  initWiFi();
   // client.unsubscribe("/hello");
-}
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-
-  // Note: Do not use the client in the callback to publish, subscribe or
-  // unsubscribe as it may cause deadlocks when other things arrive while
-  // sending and receiving acknowledgments. Instead, change a global variable,
-  // or push to a queue and handle it in the loop after calling `client.loop()`.
 }
 
 void IRAM_ATTR Mount(){
@@ -66,24 +34,45 @@ void setup() {
   pinMode(INPUT_PIN, INPUT);
   pinMode(OUTPUT_PIN, OUTPUT);
 
-
-  client.begin(mqtt_broker, MQTT_PORT, net);
-  client.onMessage(messageReceived);
-
   connect();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  client.loop();
   delay(10);  // <- fixes some issues with WiFi stability
 
-  if (!client.connected()) {
-    connect();
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnected();
+    return;
   }
 
+  if(!tb.connected()){
+    if(!tb.connect(mqtt_broker,mqtt_Token,1883)){
+      return;
+    }
+  }
+
+  tb.sendTelemetryFloat("pulse", 2); //<- send value to thingboard
+
+  tb.loop();
 }
 
-void sendMassege(){
-  client.publish(mqtt_topic, "{"); //<= form it's like JSON body
+void initWiFi(){
+  WiFi.begin(ssid,password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
 }
+
+void reconnected(){
+    status = WiFi.status();
+    if(status != WL_CONNECTED){
+      WiFi.begin(ssid,password);
+      while(WiFi.status() != WL_CONNECTED){
+        delay(500);
+      }
+    }
+}
+
+
